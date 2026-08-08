e31162e6fd268d6244e3138ef8813551ac74fb70
jacobevci-lab <jacob.evci@gmail.com>
2026-08-08 12:08:56 +0300

    feat(api): wire Prisma GRC repository

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,11 +1,17 @@
 import { Module } from '@nestjs/common';
 import { ThrottlerModule } from '@nestjs/throttler';
+import { PrismaService } from './database/prisma.service';
 import { GrcController } from './grc/grc.controller';
+import { GRC_REPOSITORY, PrismaGrcRepository } from './grc/grc.repository';
 import { GrcService } from './grc/grc.service';
 
 @Module({
   imports: [ThrottlerModule.forRoot([{ ttl: 60_000, limit: 120 }])],
   controllers: [GrcController],
-  providers: [GrcService],
+  providers: [
+    PrismaService,
+    GrcService,
+    { provide: GRC_REPOSITORY, useClass: PrismaGrcRepository },
+  ],
 })
 export class AppModule {}
